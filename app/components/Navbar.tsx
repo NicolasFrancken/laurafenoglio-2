69fef86b02b8bc22b177cb5b58c89d10c4e5d497
NicolasFrancken <franckennicolas@gmail.com>
2023-11-10 13:58:59 -0300

    responsive navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between items-center px-20 py-3 fixed top-0 left-0 right-0 bg-background z-50">
+    <header className="flex justify-between items-center px-5 lg:px-20 py-3 fixed top-0 left-0 right-0 bg-background z-50">
       <Image
         src={"/images/logo.png"}
         alt="Laura Fenoglio Logo"
         width={200}
         height={50}
+        className="w-[150px] md:w-[200px] md:h-[50px]"
       />
-      <nav className="flex gap-10 text-lg">
+      <nav className="hidden md:flex gap-10 text-lg">
         <Link
           href={"#home"}
           className="hover:text-[#8a2e67] hover:transition-colors duration-150"
