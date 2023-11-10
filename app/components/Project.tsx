e3fbd378cfae40e8d6a55f3f0089b6cf550cd9b8
NicolasFrancken <franckennicolas@gmail.com>
2023-11-10 15:57:08 -0300

    responsiveness

---
+++ app/components/Project.tsx
@@ -0,0 +1,24 @@
+interface ProyectProps {
+  customClasses?: string;
+  title: string;
+  description: string;
+  background: string;
+}
+
+export default function Project({
+  customClasses,
+  title,
+  description,
+  background,
+}: ProyectProps) {
+  return (
+    <div
+      className={`${background} flex w-[280px] h-[180px]  xl:w-[350px] xl:h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md`}
+    >
+      <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
+        <h6 className="text-base md:text-xl">{title}</h6>
+        <p className="text-sm md:text-base">{description}</p>
+      </div>
+    </div>
+  );
+}
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
+import Project from "./Project";
 
 export default function Projects() {
   const [view, setView] = useState("departamentos");
@@ -25,31 +26,27 @@ export default function Projects() {
   switch (view) {
     case "departamentos":
       renderedView = (
-        <div className="grid grid-cols-2 grid-rows-2 gap-10 ">
-          <div className="flex la-aguada w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">La Aguada</h6>
-              <p>Departamentos - Unidades disponibles</p>
-            </div>
-          </div>
-          <div className="flex san-martin-design w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">San Martín Design</h6>
-              <p>Departamentos - No disponible</p>
-            </div>
-          </div>
-          <div className="flex bustillo-uno w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">Bustillo Uno</h6>
-              <p>Departamentos - Unidades disponibles</p>
-            </div>
-          </div>
-          <div className="flex carantania w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">Carantania</h6>
-              <p>Departamentos - No disponible</p>
-            </div>
-          </div>
+        <div className="grid grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 gap-5 md:gap-10 ">
+          <Project
+            title="La Aguada"
+            description="Departamentos - Disponibles"
+            background="la-aguada"
+          />
+          <Project
+            title="San Martín Design"
+            description="Departamentos - No disponible"
+            background="san-martin-design"
+          />
+          <Project
+            title="Bustillo Uno"
+            description="Departamentos - Disponibles"
+            background="bustillo-uno"
+          />
+          <Project
+            title="Carantania"
+            description="Departamentos - No disponible"
+            background="carantania"
+          />
         </div>
       );
       break;
@@ -60,56 +57,49 @@ export default function Projects() {
           <div
             className={`${
               selectedPage === 1 ? "grid" : "hidden"
-            } grid-cols-2 grid-rows-2 gap-10 `}
+            }  grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 gap-5 md:gap-10 `}
           >
-            <div className="flex lomas-del-cauquen w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Lomas del Cauquen</h6>
-                <p>Barrio abierto - Últimos lotes disponibles</p>
-              </div>
-            </div>
-            <div className="flex las-pitras w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Las Pitras</h6>
-                <p>Barrio abierto - Sin lotes disponibles</p>
-              </div>
-            </div>
-            <div className="flex entre-cerros w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Entre Cerros</h6>
-                <p>Barrio abierto - Últimos lotes disponibles</p>
-              </div>
-            </div>
-            <div className="flex altos-de-dina w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Altos de Dina Huapi</h6>
-                <p>Barrio abierto - Últimos lotes disponibles</p>
-              </div>
-            </div>
+            <Project
+              title="Lomas del Cauquen"
+              description="Barrio abierto - Lotes disponibles"
+              background="lomas-del-cauquen"
+            />
+            <Project
+              title="Las Pitras"
+              description="Barrio abierto - Sin lotes disponibles"
+              background="las-pitras"
+            />
+            <Project
+              title="Entre Cerros"
+              description="Barrio abierto - Lotes disponibles"
+              background="entre-cerros"
+            />
+            <Project
+              title="Altos de Dina Huapi"
+              description="Barrio abierto - Lotes disponibles"
+              background="altos-de-dina"
+            />
           </div>
           <div
             className={`${
               selectedPage === 1 ? "hidden" : "grid"
-            } grid-cols-2 grid-rows-2 gap-10 `}
+            }  grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 gap-5 md:gap-10 `}
           >
-            <div className="flex rincon-del-este w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Rincón del Este</h6>
-                <p>Barrio abierto - Lotes disponibles</p>
-              </div>
-            </div>
-            <div className="flex pasaje-del-este w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Pasaje del Este</h6>
-                <p>Barrio abierto - Lotes disponibles</p>
-              </div>
-            </div>
-            <div className="flex reina-mora w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-              <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-                <h6 className="text-xl">Reina Mora</h6>
-                <p>Barrio abierto - Últimos lotes disponibles</p>
-              </div>
-            </div>
+            <Project
+              title="Rincón del Este"
+              description="Barrio abierto - Lotes disponibles"
+              background="rincon-del-este"
+            />
+            <Project
+              title="Pasaje del Este"
+              description="Barrio abierto - Lotes disponibles"
+              background="pasaje-del-este"
+            />
+            <Project
+              title="Reina Mora"
+              description="Barrio abierto - Lotes disponibles"
+              background="reina-mora"
+            />
           </div>
         </>
       );
@@ -117,25 +107,22 @@ export default function Projects() {
 
     case "oficinas":
       renderedView = (
-        <div className="grid grid-cols-2 grid-rows-2 gap-10">
-          <div className="flex san-martin-design w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">San Martín Design</h6>
-              <p>Oficinas - No disponibles</p>
-            </div>
-          </div>
-          <div className="flex bariloche-center w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">Bariloche Center</h6>
-              <p>Locales y Oficinas - Unidades disponibles</p>
-            </div>
-          </div>
-          <div className="flex paseo-catedral w-[350px] h-[250px] shadow-2xl hover:opacity-80 hover:-translate-y-1 transition-all duration-150 hover:cursor-pointer rounded-md">
-            <div className="w-full bg-foreground self-end text-background px-5 p-[10px] rounded-b-md">
-              <h6 className="text-xl">Paseo Catedral</h6>
-              <p>Locales y Oficinas - No disponibles</p>
-            </div>
-          </div>
+        <div className="grid grid-cols-1 grid-rows-1 sm:grid-cols-2 sm:grid-rows-2 gap-5 md:gap-10">
+          <Project
+            title="San Martín Design"
+            description="Oficinas - No disponibles"
+            background="san-martin-design"
+          />
+          <Project
+            title="Bariloche Center"
+            description="Locales y Oficinas - Disponibles"
+            background="bariloche-center"
+          />
+          <Project
+            title="Paseo Catedral"
+            description="Locales y Oficinas - No disponibles"
+            background="paseo-catedral"
+          />
         </div>
       );
       break;
@@ -144,15 +131,15 @@ export default function Projects() {
   return (
     <section
       id="proyectos"
-      className="min-h-screen px-28 py-20 flex flex-col gap-10"
+      className="min-h-screen px-5 lg:px-20 xl:px-28 py-20 flex flex-col gap-10 "
     >
-      <h3 className="text-4xl">PROYECTOS</h3>
-      <div className="flex justify-between items-start">
-        <div className="flex flex-col items-start gap-3">
+      <h3 className="text-2xl md:text-3xl lg:text-4xl">PROYECTOS</h3>
+      <div className="flex projects justify-between items-start gap-10">
+        <div className="flex flex-col projects-buttons items-start gap-3">
           <button
-            className={`text-[#999DA9] medium text-xl pl-2 ${
+            className={`text-[#999DA9]  text-base md:text-xl pl-0 md:pl-2 ${
               view === "departamentos"
-                ? "border-l-[3px] border-[#8a2e67] text-foreground"
+                ? "border-b-[3px] md:border-b-0 md:border-l-[3px] border-[#8a2e67] text-foreground"
                 : ""
             }`}
             onClick={() => handleClick("departamentos")}
@@ -160,9 +147,9 @@ export default function Projects() {
             Departamentos
           </button>
           <button
-            className={`text-[#999DA9] medium text-xl pl-2 ${
+            className={`text-[#999DA9]  text-base md:text-xl pl-0 md:pl-2 ${
               view === "loteos"
-                ? "border-l-[3px] border-[#8a2e67] text-foreground"
+                ? "border-b-[3px] md:border-b-0 md:border-l-[3px] border-[#8a2e67] text-foreground"
                 : ""
             }`}
             onClick={() => handleClick("loteos")}
@@ -170,9 +157,9 @@ export default function Projects() {
             Loteos
           </button>
           <button
-            className={`text-[#999DA9] medium text-xl pl-2 ${
+            className={`text-[#999DA9] text-base md:text-xl pl-0 md:pl-2 ${
               view === "oficinas"
-                ? "border-l-[3px] border-[#8a2e67] text-foreground"
+                ? "border-b-[3px] md:border-b-0 md:border-l-[3px] border-[#8a2e67] text-foreground"
                 : ""
             }`}
             onClick={() => handleClick("oficinas")}
@@ -180,19 +167,19 @@ export default function Projects() {
             Oficinas
           </button>
         </div>
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col justify-center items-center  gap-10">
           {renderedView}
-          <div className="flex justify-between items-center gap-20 w-full">
+          <div className="flex justify-between items-center gap-5 md:gap-20 w-full">
             <button
-              className="flex justify-center items-center w-[200px] py-2 bg-foreground text-background text-lg rounded-md hover:bg-[#8a2e67] hover:text-background hover:scale-110 transition-all duration-150 disabled:opacity-50 disabled:hover:scale-100 disabled:hover:bg-foreground"
+              className="flex justify-center items-center w-[120px] md:w-[200px] px-4 py-2 bg-foreground text-background text-lg rounded-md hover:bg-[#8a2e67] hover:text-background hover:scale-110 transition-all duration-150 disabled:opacity-50 disabled:hover:scale-100 disabled:hover:bg-foreground"
               onClick={handleVolverClick}
               disabled={
                 view === "departamentos" || view === "oficinas" ? true : false
               }
             >
-              <BiLeftArrowAlt className="mr-6 w-6 h-6" /> Volver
+              <BiLeftArrowAlt className="mr-6 md:w-6 md:h-6" /> Volver
             </button>
-            <div className="flex gap-3">
+            <div className="hidden sm:flex gap-3">
               <div
                 className={`w-2 h-2 rounded-full ${
                   selectedPage === 1 ? "bg-foreground" : "bg-foreground/50"
@@ -205,14 +192,14 @@ export default function Projects() {
               ></div>
             </div>
             <button
-              className="flex justify-center items-center w-[200px] py-2 bg-foreground text-background text-lg rounded-md hover:bg-[#8a2e67] hover:text-background hover:scale-110 transition-all duration-150 disabled:opacity-50 disabled:hover:scale-100 disabled:hover:bg-foreground"
+              className="flex justify-center items-center w-[120px] md:w-[200px] px-4 py-2 bg-foreground text-background text-lg rounded-md hover:bg-[#8a2e67] hover:text-background hover:scale-110 transition-all duration-150 disabled:opacity-50 disabled:hover:scale-100 disabled:hover:bg-foreground"
               onClick={handlePasarClick}
               disabled={
                 view === "departamentos" || view === "oficinas" ? true : false
               }
             >
               Pasar
-              <BiRightArrowAlt className="ml-6 w-6 h-6" />
+              <BiRightArrowAlt className="ml-6 md:w-6 md:h-6" />
             </button>
           </div>
         </div>
